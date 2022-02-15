fb4f81e220d30b64fb379c4964a9d275bddeb390
hayley-leblanc <hleblanc@utexas.edu>
2022-02-15 12:40:38 -0600

    Scripts for tests

---
+++ fs/hayleyfs/scripts/test_mkdir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cwd=$(dirname "$0")
+
+$cwd/load.sh
+
+sudo mkdir /mnt/pmem/foo
+sudo stat /mnt/pmem/foo
+
+sudo mkdir /mnt/pmem/bar
+sudo stat /mnt/pmem/bar
+
+sudo mkdir /mnt/pmem/foo/baz
+sudo stat /mnt/pmem/foo/baz
+
+$cwd/unload.sh
